43af3d71583e07d9ef5da2d5745d91ede6b618ae
Amanpawer <amanpawarap0912@gmail.com>
2025-07-19 03:02:58 -0600

    Add files via upload

---
+++ task 01.py
@@ -0,0 +1,8 @@
+#hi my english is very poor so egnor if there is any grammatical mistake
+print("note: enter only numerical values ")
+a = int (input("Enter your first number"))
+b = int (input("Enter your second number"))
+print ("addiction:",a+b)
+print ("Subtraction:",a-b)
+print ("Multiplication:",a*b)
+print ("Division:",a/b)
